0c31d8202f866376e35a66497a5298eb046ae6f1
crassSandwich <christopherjanidlo@gmail.com>
2019-10-29 16:32:19 -0400

    Hot reloading code woah mama

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-### CLEAN
-rm -rf public
-
-
-### BUILD
-mkdir -p public
+rm -rf build
+mkdir build
 
 # compile ink to javascript
-./lib/inklecate -o public/story.json src/story.ink
-tr -cd '[:print:]' < public/story.json > public/storyTrimmed.json # clean unprintable characters
-{ echo -n "const storyContent = "; cat public/storyTrimmed.json | tr -d "[:space:]"; } > public/story.js
-rm public/story.json public/storyTrimmed.json
+./lib/inklecate -o build/story.json src/story.ink
+tr -cd '[:print:]' < build/story.json > build/storyTrimmed.json # clean unprintable characters
 
-# copy every other file
+{ echo -n "const storyContent = "; cat build/storyTrimmed.json; } > build/story.js
+
+mkdir -p public
+
 cp lib/ink.js public
 cp src/main.js public
 cp src/index.html public
+cp build/story.js public
+
+rm -rf build
